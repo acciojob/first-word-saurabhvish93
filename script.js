526d1161db6ceb4f3944d8eef9a69bf620d3c748
Saurabh Kumar Vishwakarma <137218388+saurabhvish93@users.noreply.github.com>
2024-09-22 22:39:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 function firstWord(s) {
   // your code here
+	s = s.trim(); 
+	const spaceIndex = s.indexOf(' ');
+	if(spaceIndex === -1) {
+		return s;
+	}
+	return s.substring(0, spaceIndex);
 }
 
 // Do not change the code below
